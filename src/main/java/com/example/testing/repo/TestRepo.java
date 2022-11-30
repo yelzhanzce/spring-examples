cd482ba99aa25897e4c74d0b72fdb96ae1e740af
yelzhanzce <yeelzhankz@gmail.com>
2022-12-01 01:29:57 +0600

    testRepo: removed unused imports

--- src/main/java/com/example/testing/repo/TestRepo.java
+++ src/main/java/com/example/testing/repo/TestRepo.java
@@ -1,9 +1,5 @@
 package com.example.testing.repo;
 
-import com.example.testing.entity.BaseEntity;
-
-import java.util.List;
-
 public class TestRepo {
 
 //    public List<BaseEntity> getAllEntities(String name, )
